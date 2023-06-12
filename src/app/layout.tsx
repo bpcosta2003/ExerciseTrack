2db40e0597de61d5e5717c7241534214af4e5d05
bpcosta2003 <bruno.p.costa.2003@gmail.com>
2023-06-12 16:33:15 -0300

    tailwindcssUpdate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 "use client";
 import {Inter} from "next/font/google";
 import {ReactNode} from "react";
-import "../styles/input.css";
+import "../styles/globals.css";
 
 const inter = Inter({weight: "700", subsets: ["latin"]});
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /* eslint-disable prettier/prettier */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
